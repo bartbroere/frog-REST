25ad58265991db57a77763de67d02766cf77e77a
Bart Broere <mail@bartbroere.eu>
2020-06-16 14:46:34 +0200

    Add organisation and person finder

---
+++ server.py
@@ -0,0 +1,45 @@
+import itertools
+import json
+
+from frog import Frog, FrogOptions
+from flask import Flask, request
+
+
+frog = Frog(FrogOptions())
+app = Flask(__name__)
+
+
+@app.route('/process', methods=['GET', 'POST'])
+def process():
+    if request.method == 'POST':
+        return json.dumps(frog.process(request.form.get('sentence')))
+    else:
+        return 'Perform a POST request with a sentence parameter to get the FROG-tokenized and annotated sentence back'
+
+
+@app.route('/organisations', methods=['GET', 'POST'])
+def organisations():
+    organisations = []
+    if request.method == 'POST':
+        processed_document = frog.process(request.form.get('sentence'))
+        for is_organisation, organisation in itertools.groupby(processed_document,
+                                                               key=lambda x: x['ner'].endswith('ORG')):
+            if is_organisation:
+                organisations.append(" ".join(x['text'] for x in organisation))
+        return json.dumps(organisations)
+
+
+@app.route('/persons', methods=['GET', 'POST'])
+def persons():
+    persons = []
+    if request.method == 'POST':
+        processed_document = frog.process(request.form.get('sentence'))
+        for is_person, person in itertools.groupby(processed_document,
+                                                               key=lambda x: x['ner'].endswith('-PER')):
+            if is_person:
+                persons.append(" ".join(x['text'] for x in person))
+        return json.dumps(persons)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
